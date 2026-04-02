47b62d2a57a4c1d709d47ba57a9da767f6d78daa
Zoltan-LS <zoltan@layershift.me>
2026-04-02 11:09:39 +0000

    format zulip

--- apps/api/views.py
+++ apps/api/views.py
@@ -2231,7 +2231,7 @@ def wait_for_completion_and_notify(target, sites_data, start_time):
         else:
             title_prefix = "✅ "
         
-        title = f"{ticket_id} {os.getenv('ZULIP_SUBJECT', 'Monitoring')} {target_name}"
+        title = f"{ticket_id}{os.getenv('ZULIP_SUBJECT', 'Monitoring')} {target_name}"
         
         Notify.send(
             title=title,
